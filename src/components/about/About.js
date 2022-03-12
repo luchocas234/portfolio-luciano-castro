890175d8ac2e1e89defc559f886b29be9d712c3b
luchocas234 <lfcastro2911@gmail.com>
2022-03-12 03:33:08 -0300

    j

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -7,7 +7,11 @@ export default function About() {
     <div className='about-container'>
         <div className='about-desc'>
             <h3>Let me tell you something about me</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur doloremque quidem laudantium consequuntur unde labore beatae laboriosam tempora repellendus. Numquam id error dolorem eaque magnam rerum blanditiis sapiente nostrum tempore.</p>
+            <p>I'm passionate about technology and businesses. Always learning trends and updating projects, I love innovation and creative solutions.
+              <br/>
+              I have the experience from +5 years working for Entrepreneurs and Local Shops using Digital Marketing to get their goals.
+            <br/>  
+            I would like to develop webs with the newest technology for your projects. We can create uniques things, let's talk!</p>
         </div>
         <div className='about-img'>
             <img src="https://scontent.faep4-3.fna.fbcdn.net/v/t39.30808-6/274722693_10221112208022712_8183670300487655712_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=730e14&_nc_eui2=AeE8bxmcRtU5Qn6Eo1Ipu8RbZINHbTdv8QFkg0dtN2_xAT7J6lW0DhyubaGvesanpx8&_nc_ohc=oYx6Rzt0fXkAX_s3egu&_nc_ht=scontent.faep4-3.fna&oh=00_AT-d5HCpYkU8RlieMsMVHEngIpdNa1LNlRveHnqMnwQ0uA&oe=6230C126"alt="about" />
