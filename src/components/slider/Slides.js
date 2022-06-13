3b52dec7105ed88dbb8dd2581b520d02d41c0a61
luchocas234 <lfcastro2911@gmail.com>
2022-06-13 17:06:53 -0300

    j

--- src/components/slider/Slides.js
+++ src/components/slider/Slides.js
@@ -6,25 +6,25 @@ const slidesInfo = [
         src:"https://cdn.pixabay.com/photo/2020/05/30/09/53/todo-lists-5238324_1280.jpg",
         alt: "Project1",
         desc: "App - Lista de Tareas",
-        link: "www.google.com"
+        link: "https://github.com/luchocas234/app_lista_tareas"
     },
     {
         src:"https://cdn.pixabay.com/photo/2016/11/08/05/01/airplane-1807486_1280.jpg",
         alt: "Project3",
         desc: "App - Adivina el Avión",
-        link: "www.youtube.com"
+        link: "https://github.com/luchocas234/aeronauticswords"
     },
     {
         src:"https://i.ibb.co/hV9tZ0Z/2.png",
         alt: "Pilotfy",
         desc:"Content Creator - Pilotfy",
-        link: "www.youtube.com"
+        link: "https://www.instagram.com/pilotfyapp/"
     },
     {
         src:"https://i.ibb.co/kGmt7d4/Captura-de-Pantalla-2022-05-16-a-la-s-01-41-03.png",
         alt: "Pilotfy",
-        desc:"Content Creator - Alpha Pergamino",
-        link: "www.youtube.com"
+        desc:"Social Media Manager - Alpha Pergamino",
+        link: "https://www.instagram.com/alpha.pergamino/"
     }
 ]
 
@@ -33,6 +33,8 @@ const slides =slidesInfo.map( slide=> (
         <a href={slide.link}><img src={slide.src} alt={slide.alt}/></a>
         <div className="slide-desc">
             <span>{slide.desc}</span>
+            
+            
         </div>
     </div>
 ))
